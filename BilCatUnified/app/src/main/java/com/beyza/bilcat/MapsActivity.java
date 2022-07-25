352ff12c1e339e56f3fb34cd0382ba1c1b6dba43
Ege Cenberci <ege.cenberci.w@gmail.com>
2022-07-26 01:43:27 +0300

    Added finalize ping button (not functional)
    Created new layout for ping map with finalize ping button

--- BilCatUnified/app/src/main/java/com/beyza/bilcat/MapsActivity.java
+++ BilCatUnified/app/src/main/java/com/beyza/bilcat/MapsActivity.java
@@ -81,8 +81,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_generalmap);
+        if (CatList.pingClicked){
+            setContentView(R.layout.ping_map);
+        }
+        else {
+            setContentView(R.layout.activity_generalmap);
+        }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
